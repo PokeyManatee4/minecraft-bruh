bacf966581b6a65a9829077c0f22d1879ba99567
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-20 02:42:45 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
-#define _Bool bool
 int main() {
    void generate() {
   printf("Placed Block");
 }
    bool Minecraft = true;
-   if (_Bool Minecraft = true) {
+   if (boolean Minecraft = true) {
    generate();
    }
 }
